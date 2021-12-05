6bd22ceb4ededac48b63b16095f197695f633a77
Vasili Cekaskin <vasili.cekaskin@invision.de>
2021-12-05 20:47:20 +0100

    Add example lesson

--- components/editor.tsx
+++ components/editor.tsx
@@ -5,7 +5,18 @@ import "ace-builds/src-noconflict/mode-sql";
 import "ace-builds/src-noconflict/theme-tomorrow";
 
 const Editor = (props) => {
-  return <AceEditor mode="sql" theme="tomorrow" {...props} />;
+  return (
+    <div className="border-4">
+      <AceEditor
+        height="200px"
+        fontSize="18px"
+        showGutter={false}
+        mode="sql"
+        theme="tomorrow"
+        {...props}
+      />
+    </div>
+  );
 };
 
 export default Editor;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,84 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 import React from "react";
+import Image from "next/image";
 import dynamic from "next/dynamic";
+import lessonOne from "../public/images/lesson_1.png";
 
 const Editor = dynamic(import("../components/editor"), { ssr: false });
 
-function onChange(newValue) {
-  console.log("change", newValue);
-}
-
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <div></div>
-      <div>
-        <Editor onChange={onChange} name="SqlEditor" />
+    <div className="grid grid-cols-2">
+      <div className="overflow-y-scroll h-full">
+        <div className="m-6">
+          <h1 className="font-bold text-xl text-gray-600 block">Aufgabe 1</h1>
+          <p className="mt-1">
+            Sie sind Mitarbeiter der Hurensohn AG und Co GmbH. Gegeben ist
+            folgende Datenbank. Mach mal bisschen SQL
+          </p>
+          <Image
+            className="w-1/2"
+            src={lessonOne}
+            alt="lesson 1 database"
+          ></Image>
+
+          <p className="mt-1">
+            Erstelle eine SQL Anweisung die die Namen aller Adressen ausgibt.
+          </p>
+        </div>
+
+        <div>
+          <h2 className="font-bold text-xl text-gray-600 block mt-10">
+            Beispiel Ausgabe
+          </h2>
+
+          <div className="mt-6 flex justify-center">
+            <table className="table-auto border-4">
+              <th className="border-2 p-2">Name</th>
+              <tr className="border-2 p-2">
+                <td className="border-2 p-1">Berliner Allee 3</td>
+              </tr>
+              <tr className="border-2 p-2">
+                <td className="border-2 p-1">Eine dumme Straße</td>
+              </tr>
+
+              <tr className="border-2 p-2">
+                <td className="border-2 p-1">Eine schlaue Straße</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      <div className="overflow-y-scroll h-full">
+        <div className="m-6 mr-0">
+          <h1 className="font-bold text-xl text-gray-600 block">SQL Editor</h1>
+          <Editor name="SqlEditor" />
+
+          <button className="mt-10 w-full border-4">Run SQL</button>
+
+          <div>
+            <h2 className="font-bold text-xl text-gray-600 block mt-10">
+              Deine Ausgabe
+            </h2>
+
+            <div className="mt-6 flex justify-center">
+              <table className="table-auto border-4">
+                <th className="border-2 p-2">Name</th>
+                <tr className="border-2 p-2">
+                  <td className="border-2 p-1">Berliner Allee 3</td>
+                </tr>
+                <tr className="border-2 p-2">
+                  <td className="border-2 p-1">Eine dumme Straße</td>
+                </tr>
+
+                <tr className="border-2 p-2">
+                  <td className="border-2 p-1">Eine schlaue Straße</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
